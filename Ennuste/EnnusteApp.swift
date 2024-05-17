608ecaa6934c1b04390846e89027fb8e858aa9b3
Lauri Ahlfors <ahlfroslauri@gmail.com>
2024-05-17 23:17:12 +0300

    feat: initial commit

---
+++ Ennuste/EnnusteApp.swift
@@ -0,0 +1,18 @@
+//
+//  EnnusteApp.swift
+//  Ennuste
+//
+//  Created by Lauri Ahlfors on 17.5.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EnnusteApp: App {
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
